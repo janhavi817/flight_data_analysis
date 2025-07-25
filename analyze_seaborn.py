8523e94fc8e48583294cc2a302a66069f1051c54
janhavi817 <janhavisuryawanshi2@gmail.com>
2025-07-25 18:05:29 +0530

    Update analyze_seaborn.py

--- analyze_seaborn.py
+++ analyze_seaborn.py
@@ -10,20 +10,4 @@ sns.heatmap(pivot_table)
 plt.title("Passengers by Year and Month")
 plt.show()
 
-import pandas as pd
-# ['Type', 'Airline', 'Year', 'Month', 'Aircraft Number', 'Aircraft Hours',
-#        'Aircraft Kilometres', 'Passenger Number', 'Passenger Kilometers',
-#        'Seat Kilometers', 'Passenger Load Factor', 'Passenger Tonne Kilometer',
-#        'Total Tonne Kilometer', 'Available Tonne Kilometer',
-#        'Weight Load Factor']
 
-# Example dataset
-# df = pd.DataFrame({
-#     "Category": ["A", "A", "B", "B", "C", "C"],
-#     "Month": ["Jan", "Feb", "Jan", "Feb", "Jan", "Feb"],
-#     "Sales": [100, 120, 90, 110, 80, 95]
-# })
-
-# # Create pivot table
-# pivot_table = df.pivot_table(index="Category", columns="Month", values="Sales", aggfunc="sum")
-# print(pivot_table)
